1b38f2ff7e2b33d963a67d33dd090571f4485560
Rohith272 <rohithk24280@gmail.com>
2022-08-22 19:01:04 +0530

    bug fixed

--- src/app/invoice-listing/invoice-listing.component.ts
+++ src/app/invoice-listing/invoice-listing.component.ts
@@ -13,36 +13,36 @@ import { InvoiceService } from '../services/invoice.service';
   styleUrls: ['./invoice-listing.component.css']
 })
 export class InvoiceListingComponent implements OnInit {
-  opened=false;
-  selectedInvoice: InvoiceDetails | undefined ; 
-  data: InvoiceDetails[] =[];
+  opened = false;
+  selectedInvoice: InvoiceDetails | undefined;
+  data: InvoiceDetails[] = [];
 
 
-  constructor(private router:Router, private invoiceservice:InvoiceService) {
+  constructor(private router: Router, private invoiceservice: InvoiceService) {
     this.data = this.invoiceservice.getData()
     this.dataSource = new MatTableDataSource<any>(this.data);
-   }
-
-  ngOnInit(): void {
-      
   }
 
- public dataSource =new MatTableDataSource<any>([this.data]);
-  displayedColumns: string[] = ['invoiceNumber','invoiceDate','amount','employeeID']
+  ngOnInit(): void {
 
-  addInvoice(){
+  }
+
+  public dataSource = new MatTableDataSource<any>([this.data]);
+  displayedColumns: string[] = ['invoiceNumber', 'invoiceDate', 'amount', 'employeeID']
+
+  addInvoice() {
     this.router.navigate(['/invoicedetails'])
     this.invoiceservice.current = undefined
   }
-  
 
-  displayData(row:any,index:any){
+
+  displayData(row: any, index: any) {
     this.selectedInvoice = row;
-    this.opened=!this.opened;
+    this.opened = !this.opened;
     if (this.selectedInvoice)
-      this.invoiceservice.update(this.selectedInvoice,index)
+      this.invoiceservice.update(this.selectedInvoice, index)
     //this.router.navigate(['/invoicedetails'])
   }
 
-  
- }
+
+}
